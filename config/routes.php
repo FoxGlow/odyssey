1e6e758371d40da44b955a0f22942630301eaa3c
FoxGlow <hugolinma@gmail.com>
2021-02-19 00:29:04 +0100

    Add some routes

--- config/routes.php
+++ config/routes.php
@@ -8,6 +8,15 @@
  * Here you can define routes.
  *  
  */
+// Static pages
 $r->get('/[index]', 'static#home');
 $r->get('/getting-started', 'static#gettingStarted');
 $r->get('/credits', 'static#credits');
+
+// User
+$r->addGroup('/user', function (FastRoute\RouteCollector $r) {
+    $r->get('/login', 'user#login');
+    $r->get('/register', 'user#register');
+    $r->post('/login', 'user#authenticate');
+    $r->post('/register', 'user#create');
+});
