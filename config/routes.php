e33c77f7da5db384f05d8f08d742675b0d5f1de8
FoxGlow <hugolinma@gmail.com>
2021-02-19 00:13:15 +0100

    Add some routes

---
+++ config/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * -------------------------------------------------------------------------+
+ * ROUTE DEFINITIONS                                                        |
+ * -------------------------------------------------------------------------+
+ * 
+ * Here you can define routes.
+ *  
+ */
+$r->get('/[index]', 'static#home');
+$r->get('/getting-started', 'static#gettingStarted');
+$r->get('/credits', 'static#credits');
