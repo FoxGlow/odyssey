c4bd30f52239036a5045955d854139c9a42f3863
FoxGlow <hugolinma@gmail.com>
2021-02-23 23:45:02 +0100

    Update user controller

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -7,6 +7,7 @@
 
 namespace App\Controllers;
 
+use App\Entities\UserEntity;
 use Core\Controller\BaseController;
 
 class UserController extends BaseController {
@@ -15,16 +16,24 @@ class UserController extends BaseController {
         echo $this->container->get('twig')->render('/pages/login.html.twig');
     }
 
-    public function authenticate() {
-        // TODO
+    public function authenticate(string $mail_address, string $password) {
+        $user_entity = new UserEntity;
+        $res = $user_entity->login($mail_address, $password);
+        if (!is_null($res)) {
+            
+        }
+        else {
+            // TODO
+        }
     }
 
     public function register() {
         echo $this->container->get('twig')->render('/pages/register.html.twig');
     }
 
-    public function create() {
-        //TODO
+    public function create(string $mail_address, string $firstname, string $lastname, string $password, string $password_confirm) {
+        $user_entity = new UserEntity;
+        $res = $user_entity->register($firstname, $lastname, $mail_address, $password);
     }
 
 }
