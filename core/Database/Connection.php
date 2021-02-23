ee085dbfb17300a66b60ae7e78e813ba0e6e2651
FoxGlow <hugolinma@gmail.com>
2021-02-23 23:48:53 +0100

    Small changes in connection class

--- core/Database/Connection.php
+++ core/Database/Connection.php
@@ -31,8 +31,8 @@ class Connection {
                 self::$database_instance = new \PDO($db_type . ':host=' . $db_host . 
                     ';dbname=' . $db_name . ';charset=' . $db_charset, 
                     $db_username, $db_password, array(
-                        \PDO::ATTR_EMULATE_PREPARES => false,
-                        \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+                        \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
                     ));
             }
             catch (\Exception $e) {
