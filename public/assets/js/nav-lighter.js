d25049b188a3b055d4ddba6fcac7ca63ed9b2ad8
FoxGlow <hugolinma@gmail.com>
2021-03-01 18:42:46 +0100

    Add javascript navigation lighter

---
+++ public/assets/js/nav-lighter.js
@@ -0,0 +1,31 @@
+const main = () => {
+    const full_path_name = window.location.pathname;
+    const splited_path_name = full_path_name.split('/');
+    splited_path_name.shift();
+
+    const home = document.getElementById('nav-home');
+    const getting_started = document.getElementById('nav-getting-started');
+    const credits = document.getElementById('nav-credits');
+    const login = document.getElementById('nav-login');
+    const register = document.getElementById('nav-register');
+
+    switch (splited_path_name[0]) {
+        case "":
+            home.classList.add('selected');
+            break;
+        case "getting-started":
+            getting_started.classList.add('selected');
+            break;
+        case "credits":
+            credits.classList.add('selected');
+            break;
+        case "user":
+            if (splited_path_name[1] === 'login')
+                login.classList.add('selected');
+            else if (splited_path_name[1] === 'register')
+                register.classList.add('selected');
+            break;
+    }
+}
+
+window.addEventListener("load", main);
