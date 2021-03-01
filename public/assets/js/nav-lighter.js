d323edd6226df3b97319374a518232e7db8da886
FoxGlow <hugolinma@gmail.com>
2021-03-01 21:56:52 +0100

    Complete navigation lighter

--- public/assets/js/nav-lighter.js
+++ public/assets/js/nav-lighter.js
@@ -8,6 +8,8 @@ const main = () => {
     const credits = document.getElementById('nav-credits');
     const login = document.getElementById('nav-login');
     const register = document.getElementById('nav-register');
+    const project_list = document.getElementById('nav-project-list');
+    const project_create = document.getElementById('nav-project-create');
 
     switch (splited_path_name[0]) {
         case "":
@@ -24,6 +26,12 @@ const main = () => {
                 login.classList.add('selected');
             else if (splited_path_name[1] === 'register')
                 register.classList.add('selected');
+            break;
+        case "project":
+            if (splited_path_name[1] === 'new')
+                project_create.classList.add('selected');
+            else
+                project_list.classList.add('selected');
             break;
     }
 }
