566185dde28d62672c36ee76e4f1881b501fcbb7
FoxGlow <hugolinma@gmail.com>
2021-03-02 19:37:45 +0100

    Add associate entity

---
+++ app/Entities/AssociateEntity.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Associate entity
+ * 
+ * @author Hugolin Mariette
+ */
+
+namespace App\Entities;
+
+use Core\Entity\BaseEntity;
+
+class AssociateEntity extends BaseEntity {
+
+    public function add(int $userId, int $projectId) {
+        $request = 'INSERT INTO associe(ref_utilisateur, ref_projet)
+            VALUES (:userId, :projectId)';
+        $stmt = $this->db_connection::getInstance()->prepare($request);
+        $stmt->bindValue(':userId', $userId);
+        $stmt->bindValue(':projectId', $projectId);
+        $res = $stmt->execute();
+        return $res;
+    }
+
+    public function remove() {
+        // TO DO
+    }
+
+}
