a157929ccdfe5326469200059e62b445c782b6db
FoxGlow <hugolinma@gmail.com>
2021-03-03 19:09:00 +0100

    Update tables

--- database/tables.sql
+++ database/tables.sql
@@ -1,22 +1,24 @@
 CREATE TABLE utilisateur(
 id_utilisateur int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-nom VARCHAR (20), 
-prenom VARCHAR (20), 
-mail VARCHAR (50),
-mot_de_passe VARCHAR(255) NOT NULL);
+nom VARCHAR (255), 
+prenom VARCHAR (255), 
+mail VARCHAR (255),
+mot_de_passe VARCHAR(255) NOT NULL
+UNIQUE KEY unique_email (mail)
+);
 
 
 CREATE TABLE projet(
 id_projet int PRIMARY KEY NOT NULL AUTO_INCREMENT, 
-nom VARCHAR (20), 
-description_projet VARCHAR(255) NOT NULL,
+nom VARCHAR (255), 
+description VARCHAR(255) NOT NULL,
 ref_chef int NOT NULL,
 CONSTRAINT FK_PROJET_REF_CHEF FOREIGN KEY (ref_chef) REFERENCES utilisateur(id_utilisateur));
 
 
 CREATE TABLE message(
 id_message int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-texte VARCHAR (300),
+texte VARCHAR (255),
 date_ date,
 ref_utilisateur int NOT NULL,
 ref_projet int NOT NULL,
@@ -25,7 +27,7 @@ CONSTRAINT FK_REF_PROJET FOREIGN KEY (ref_projet) REFERENCES projet(id_projet));
 
 CREATE TABLE notification(
 id_notification int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-texte VARCHAR (300),
+texte VARCHAR (255),
 date_ date,
 ref_utilisateur int NOT NULL,
 ref_projet int NOT NULL,
@@ -40,11 +42,12 @@ CREATE TABLE associe (
         REFERENCES utilisateur (id_utilisateur),
     CONSTRAINT FK_ASSOCIE_REF_PROJET FOREIGN KEY (ref_projet)
         REFERENCES projet (id_projet)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE feedback(
 id_feedback int PRIMARY KEY NOT NULL AUTO_INCREMENT, 
-message VARCHAR (500) NOT NULL, 
+message VARCHAR (255) NOT NULL, 
 ref_projet int NOT NULL,
 CONSTRAINT FK_FEEDBACK_REF_PROJET FOREIGN KEY (ref_projet) REFERENCES projet(id_projet));
 
@@ -80,45 +83,45 @@ CONSTRAINT FK_MCF_REF_PROJET FOREIGN KEY (ref_projet) REFERENCES projet(id_proje
 
 CREATE TABLE Conseil (
 id_conseil int PRIMARY KEY NOT NULL AUTO_INCREMENT, 
-texte VARCHAR (500) NOT NULL, 
-type VARCHAR (30) NOT NULL,
+texte VARCHAR (255) NOT NULL, 
+type VARCHAR (255) NOT NULL,
 ref_utilisateur int NOT NULL,
 CONSTRAINT FK_CONSEIL_REF_PROJET FOREIGN KEY (ref_utilisateur) REFERENCES utilisateur(id_utilisateur));
 
 CREATE TABLE piscine(
 id_piscine int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-nom VARCHAR (30) NOT NULL,
+nom VARCHAR (255) NOT NULL,
 ref_bmpn int NOT NULL,
 CONSTRAINT FK_PISCINE_REF_BPMN FOREIGN KEY (ref_bmpn) REFERENCES bpmn(id_bpmn));
 
 CREATE TABLE user_story(
 id_user_story int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-contenu VARCHAR (300) NOT NULL,
+contenu VARCHAR (255) NOT NULL,
 ref_st int NOT NULL,
 CONSTRAINT FK_USER_STORY_REF_ST FOREIGN KEY (ref_st) REFERENCES story_map(id_story_map));
 
 CREATE TABLE epic(
 id_epic int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-evenement VARCHAR (300) NOT NULL,
+evenement VARCHAR (255) NOT NULL,
 ref_st int NOT NULL,
 CONSTRAINT FK_EPIC_REF_ST FOREIGN KEY (ref_st) REFERENCES story_map(id_story_map));
 
 CREATE TABLE acteur(
 id_acteur int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-nom VARCHAR(30) NOT NULL,
+nom VARCHAR(255) NOT NULL,
 ref_mcf int,
 CONSTRAINT FK_ACTEUR_REF_MCF FOREIGN KEY (ref_mcf) REFERENCES MCF(id_mcf));
 
 CREATE TABLE activite_mcf(
 id_activite_mcf int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-nom VARCHAR(300) NOT NULL,
+nom VARCHAR(255) NOT NULL,
 ref_mcf int,
 CONSTRAINT FK_ACTIVITE_MCF_REF_MCF FOREIGN KEY (ref_mcf) REFERENCES MCF(id_mcf));
 
 CREATE TABLE flux(
 id_flux int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-nom VARCHAR(300) NOT NULL,
-direction VARCHAR(300) NOT NULL,
+nom VARCHAR(255) NOT NULL,
+direction VARCHAR(255) NOT NULL,
 ref_acteur int,
 ref_activite int,
 CONSTRAINT FK_FLUX_REF_ACTEUR FOREIGN KEY (ref_acteur) REFERENCES acteur(id_acteur),
