501f1ac6adb39188b645aa803612c6cf20230f81
FoxGlow <hugolinma@gmail.com>
2021-03-20 14:12:59 +0100

    Update advices

--- database/advices.sql
+++ database/advices.sql
@@ -9,7 +9,7 @@ interne d’un autre SI, on ne sait que ce qu’on échange avec lui. C’est po
 INSERT INTO conseil (texte,type) VALUES ("Un flux de message est la transmission en une seule fois , d'une information
 structurée, entre un émetteur (qui en dispose) et un récepteur (qui en a besoin). Le message est l'information structurée que porte le flux." , "MCF");
 
-INSERT INTO conseil (texte,type) VALUES ("Dans le MCF on  fait apparaitre uniquement les acteurs externes et les sous-domaines, mais pas les acteurs internes, car les 
+INSERT INTO conseil (texte,type) VALUES ("Dans le MCF on fait apparaitre uniquement les acteurs externes et les sous-domaines, mais pas les acteurs internes, car les 
 acteurs internes font partie de la solution actuelle et sont indépendants du périmètre du SI.", "MCF" );
 
 INSERT INTO conseil (texte,type) VALUES ("Les sous-domaines sont des objectifs et sont libellés par un verbe à l’infinitif", "MCF" );
@@ -21,16 +21,15 @@ INSERT INTO conseil (texte,type) VALUES ("Les propriétés servant d'identifiant
 liste des propriétés ou précédées d'un dièse (#), pour les distinguer des autres propriétés.", "MCD");
 
 INSERT INTO conseil (texte,type) VALUES ("Attention Si deux entités partagent la même propriété identifiante, alors il n'y a qu'une seule
-entité et Si la même propriété peut être rattachée à des identifiants différents, c'est qu'il s'agit
+entité et si la même propriété peut être rattachée à des identifiants différents, c'est qu'il s'agit
 en réalité de deux propriétés différentes, qu'il faut redéfinir", "MCD" );
-
 
 INSERT INTO conseil (texte,type) VALUES ("Sur le BPMN, l’évènement doit pouvoir être retrouvé sur le BPMN . la transition également sous la forme
 d’une activité." , "CVO" );
 
 INSERT INTO conseil (texte,type) VALUES ("Un cvo est un digramme qui compte un unique début et une ou plusieurs fin ", "CVO" );
 
-INSERT INTO conseil (texte,type) VALUES ("Un cvo comporte Des évènements internes ou externes. Les évènements externes sont des flux de message
+INSERT INTO conseil (texte,type) VALUES ("Un cvo comporte des évènements internes ou externes. Les évènements externes sont des flux de message
 apparaissant dans le MFC et le BPMN.
 " , "CVO" );
 
@@ -41,7 +40,6 @@ différents chemins possibles" , "CVO" );
 INSERT INTO conseil (texte,type) VALUES ("Sur l’axe horizontal, elle comporte le flot de narration suivant les
 étapes du processus-métier (épics). Sur l’axe vertical, elle regroupe les besoins par objectifs, du plus
 prioritaire au moins prioritaires" , "STORY-MAP");
-
 
 INSERT INTO conseil (texte,type) VALUES ("une story map comporte un flot de narration qui permet de raconter l’histoire de l’utilisateur, qui va se servir du
 produit ou de la fonctionnalité. L’enchainement des différentes activités se rapproche du processus-métier." , "STORY-MAP" );
