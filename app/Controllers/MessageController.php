32b38e753434352e02c2341b54a65b6685e355e4
FoxGlow <hugolinma@gmail.com>
2021-03-21 02:07:51 +0100

    Add permission check to delete message

--- app/Controllers/MessageController.php
+++ app/Controllers/MessageController.php
@@ -27,9 +27,11 @@ class MessageController extends AppController {
         if (!$this->isUserAssociatedTo($projectId))
             $this->redirect('/project/list');
         
-        // VERIF
         $message_entity = new MessageEntity;
-        $res = $message_entity->delete($messageId);
+        $message = $message_entity->get($messageId);
+        if ($this->isUserLeaderOf($projectId) || 
+            $this->sessionGet('userId') == $message['ref_utilisateur'])
+                $res = $message_entity->delete($messageId);
 
         $this->redirect('/project/view/' . $projectId);
     }
